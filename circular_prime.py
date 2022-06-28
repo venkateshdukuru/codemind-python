ba77733103053261c2780d5d54f00cc66557a3a5
Venkatesh <98198945+venkateshdukuru@users.noreply.github.com>
2022-06-28 23:08:25 +0530

    n=int(input()) temp=n r=0 while n:     d=n%10     r=r*10+d     n=n//10 n=temp def prime(n):     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False             break     else:         return True
    if prime(n) and prime(r):
        print("circular prime")
    elif prime(n) and not prime(r):
        print("prime but not a circular prime")
    else:
        print("not prime")

--- circular_prime.py
+++ circular_prime.py
@@ -1,38 +1,22 @@
-def fun(n):
+n=int(input())
+temp=n
+r=0
+while n:
+    d=n%10
+    r=r*10+d
+    n=n//10
+n=temp
+def prime(n):
     for i in range(2,int(n**0.5)+1):
         if n%i==0:
             return False
+            break
     else:
         return True
-n=int(input())
-rem=0
-if fun(n):
-    while(n):
-        d=n%10
-        n=n//10
-        rem=rem*10+d
-    if fun(rem):
-        print('circular prime')
-    else:
-        print('prime but not a circular prime')
+
+if prime(n) and prime(r):
+    print("circular prime")
+elif prime(n) and not prime(r):
+    print("prime but not a circular prime")
 else:
-    print('not prime')
-def fun(n):
-    for i in range(2,int(n**0.5)+1):
-        if n%i==0:
-            return False
-    else:
-        return True
-n=int(input())
-rem=0
-if fun(n):
-    while(n):
-        d=n%10
-        n=n//10
-        rem=rem*10+d
-    if fun(rem):
-        print('circular prime')
-    else:
-        print('prime but not a circular prime')
-else:
-    print('not prime')
+    print("not prime")
