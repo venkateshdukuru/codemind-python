8e2f4fbf0d65e428617e29fd9cf7522c28a28086
Venkatesh <98198945+venkateshdukuru@users.noreply.github.com>
2022-07-16 18:52:43 +0530

    def fun(n):     c=0     if n==0:         return 1     if n<0:         n=-n     while n:         c+=1         n=n//10     return c n=int(input()) a=list(map(int,input().split())) b=[] for i in a:     b.append(fun(i)) x=max(b) for i in a:     if fun(i)==x:         print(i,end=' ')

---
+++ maximum_digit_numbers_count.py
@@ -0,0 +1,19 @@
+def fun(n):
+    c=0
+    if n==0:
+        return 1
+    if n<0:
+        n=-n
+    while n:
+        c+=1
+        n=n//10
+    return c
+n=int(input())
+a=list(map(int,input().split()))
+b=[]
+for i in a:
+    b.append(fun(i))
+x=max(b)
+for i in a:
+    if fun(i)==x:
+        print(i,end=' ')
